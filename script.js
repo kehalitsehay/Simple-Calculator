79f0dfb2250e60c06b7ceb66e626f5c2e80bd4e6
kehali Tsehay <87518023+kehalitsehay@users.noreply.github.com>
2023-12-18 09:20:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+        const buttons = document.querySelectorAll(".number, .operator, #clear, #equals, #backspace");
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                if (button.id === "clear") {
+                    display.value = "";
+                } else if(button.id === "squareroot"){
+                    display.value = Math.sqrt(display.value)
+                }else if (button.id === "equals") {
+                    try {
+                        display.value = eval(display.value);
+                    }
+                    catch (error) {
+                        display.value = "Error";
+                    }
+                } else if (button.id === "backspace") {
+                    display.value = display.value.slice(0, -1);
+
+                }
+                else {
+                    display.value += button.textContent;
+                }
+            });
+        });
